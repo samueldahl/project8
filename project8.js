d7180611789c46dbdb0f706b6f0ebb8407eba7ee
samuel dahl <samjoedahl@gmail.com>
2018-04-16 21:40:01 -0600

    Read into some pug documentation and figured out how to make anchor tags work. Next step is to figure out how to send javascript n stuff.

--- project8.js
+++ project8.js
@@ -10,17 +10,22 @@ app.set('view engine', 'Pug');
 
 app.get('/', function (req, res) {
   res.render('index');
+  console.log('Index page request fulfilled');
 })
 app.get('/edituser', function (req, res) {
   res.render('editUser');
+  console.log('Edit user page request fufilled');
 })
 app.get('/createuser', function (req, res) {
   res.render('createUser');
+  console.log('Create User page request fufilled');
 })
 app.post('/', function(req, res) {
   console.log('Yo, I got a post request. Behringer is great.');
 })
 
-app.listen(3000);
+var port = 3000;
 
-console.log('listening on port 3000');
+app.listen(port);
+
+console.log('listening on port ' + port);
