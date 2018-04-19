50c0d5b2269ce57129e2cbfa21245b1dffc8106d
samuel dahl <samjoedahl@gmail.com>
2018-04-19 17:44:16 -0600

    Now I can add users, time to add delete user

--- project8.js
+++ project8.js
@@ -4,14 +4,17 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 
 let app = express();
+var jsonParser = bodyParser.json()
 
-app.set('views', './views');
 app.set('view engine', 'Pug');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('./views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', function (req, res) {
   //Do something here with the file system that allows pug to access a JSON object I think?
   var obj = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-  console.log(obj);
   res.render('index', {data: obj});
   console.log('Index page request fulfilled');
 })
@@ -24,8 +27,26 @@ app.get('/createuser', function (req, res) {
   res.render('createUser');
   console.log('Create User page request fufilled');
 })
-app.post('/', function(req, res) {
-  console.log('Yo, I got a post request. Behringer is great.');
+
+
+
+app.post('/userTarget', function(req, res) {
+  var obj = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+  let newBoi = {
+    name:req.body.name,
+    id:req.body.id,
+    email:req.body.email,
+    age:req.body.age
+  }
+  console.log(newBoi);
+  obj.push(req.body);
+  fs.writeFile('data.json', JSON.stringify(obj));
+  console.log('Create post');
+  res.redirect('/');
+})
+app.post('/editUser', jsonParser, function(req, res) {
+  console.log('Edit post');
+  res.redirect();
 })
 
 var port = 3000;
